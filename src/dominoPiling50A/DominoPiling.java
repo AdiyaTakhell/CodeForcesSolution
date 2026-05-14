832064b1c6c07a0cbfeff92dccbedec2ee572a8e
adiya <adiyatakhellambam@gmail.com>
2026-05-14 13:45:41 +0530

    Solved the problem 50A

---
+++ src/dominoPiling50A/DominoPiling.java
@@ -0,0 +1,17 @@
+package dominoPiling50A;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        System.out.println((m * n) / 2);
+
+    }
+}
+
